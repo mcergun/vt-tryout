d233f4c3a255ec605b39a85eaf6ca6f5d7e7c368
Mert Can Ergun <mcergun90@gmail.com>
2018-02-24 22:50:56 +0300

    added small test program

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,22 @@
 #include <Key.h>
 #include <OutputChannel.h>
 #include <cstdio>
+#include <cstring>
 
 int main()
 {
 	OutputChannel oc;
+	KeyConverter kc;
+	LineBuffer lb;
 	char buf[256] = {0};
 	
-	oc.Read(buf);
-	printf("%s\n", buf);
+	oc.Initialize();
+	while (1)
+	{
+		oc.Read(buf);
+		Key &k = kc.ToKey(buf);
+		k.Execute(lb, oc);
+		oc.FlushBuffers();
+	}
 	return 0;
 }
